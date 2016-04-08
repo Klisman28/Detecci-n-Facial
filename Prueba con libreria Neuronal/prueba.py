5100bd645bab01365e8620a07cafabe493ec7d8d
Klisman28 <klisoved28@gmail.com>
2016-04-07 23:51:52 -0600

    Prueba

---
+++ Prueba con libreria Neuronal/prueba.py
@@ -0,0 +1,16 @@
+importar numpy como np
+importación Neurolab como nl
+importación numpy.random como rand
+
+
+# Creación de red con 150*150=1250 pixeles entradas y 10 neuronas
+# 10 neuronas de la capa oculta, 1 neurona de salida
+# 3 capa de salida 
+neta = nl.net.newc ([[ 1,0,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1 ], [ 1,0,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1 ],  [10 ,  3 ] )
+# Tren con el artículo: Ganador conciencia Take All algoritmo (CWTA)
+
+>>> neta . ci
+
+>>> neta . co
+
+>>> len ( netas . capas )
